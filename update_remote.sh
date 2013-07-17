4ac0ff8f12856fd06321057da5829ea4e50c0a2a
João Sampaio <jpmelos@gmail.com>
2013-07-17 16:07:43 -0300

    Force updates if needed.

--- update_remote.sh
+++ update_remote.sh
@@ -4,7 +4,7 @@ for dir in $1/[[:lower:]]*.git; do
 	if [ -d "$dir" ]; then
 		echo "Updating $dir"
 		cd "$dir"
-		git push --all $2
-		git push --tags $2
+		git push -f --all $2
+		git push -f --tags $2
 	fi
 done
