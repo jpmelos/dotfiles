54a9704554792378e2e6f9499f8339cf1495d5f3
João Sampaio <jpmelos@gmail.com>
2016-07-08 18:44:27 -0300

    Some improvements
    Signed-off-by: João Sampaio <jpmelos@gmail.com>

--- dotfiles.py
+++ dotfiles.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-from subprocess import check_output as proc_run, DEVNULL, PIPE
-from shlex import split
 from shutil import copy, copytree, rmtree
-from os import getenv, chdir
+from os import getenv
 from os.path import dirname, abspath, isdir, join, exists
 
 
@@ -12,39 +10,52 @@ BASE_DIR = dirname(abspath(__file__))
 
 
 files = [
-    ('.fonts', '.fonts'),
-    ('.gitconfig', '.gitconfig'),
-    ('localserver.conf', 'localserver.conf'),
-    ('.mybashrc', '.mybashrc'),
-    ('.mybash_profile', '.mybash_profile'),
-    ('Preferences.sublime-settings', '.config/sublime-text-3/Packages/User/Preferences.sublime-settings'),
-    ('HTML.sublime-settings', '.config/sublime-text-3/Packages/User/HTML.sublime-settings'),
-    ('Sass.sublime-settings', '.config/sublime-text-3/Packages/User/Sass.sublime-settings'),
-    ('JavaScript.sublime-settings', '.config/sublime-text-3/Packages/User/JavaScript.sublime-settings'),
-    ('JSON.sublime-settings', '.config/sublime-text-3/Packages/User/JSON.sublime-settings'),
-    ('Python.sublime-settings', '.config/sublime-text-3/Packages/User/Python.sublime-settings'),
-    ('.tmux.conf', '.tmux.conf'),
-    ('.vimrc', '.vimrc'),
+    '.fonts',
+    '.gitconfig',
+    'localserver.conf',
+    '.mybashrc',
+    '.mybash_profile',
+    '.tmux.conf',
+    '.vimrc',
+
+    # Sublime Text files
+    ('Preferences.sublime-settings',
+     '.config/sublime-text-3/Packages/User/Preferences.sublime-settings'),
+    ('HTML.sublime-settings',
+     '.config/sublime-text-3/Packages/User/HTML.sublime-settings'),
+    ('Sass.sublime-settings',
+     '.config/sublime-text-3/Packages/User/Sass.sublime-settings'),
+    ('JavaScript.sublime-settings',
+     '.config/sublime-text-3/Packages/User/JavaScript.sublime-settings'),
+    ('JSON.sublime-settings',
+     '.config/sublime-text-3/Packages/User/JSON.sublime-settings'),
+    ('Python.sublime-settings',
+     '.config/sublime-text-3/Packages/User/Python.sublime-settings'),
 ]
 
-
-def source(filename):
-    SOURCE = 'source ~/.my{filename}'.format(filename=filename)
-    with open(join(HOME_DIR, '.{filename}'.format(filename=filename)), mode='r') as fp:
-        content = fp.read()
-    if SOURCE not in content:
-        with open(join(HOME_DIR, '.{filename}'.format(filename=filename)), mode='a') as fp:
-            fp.write('\n' + SOURCE + '\n')
-
-
 for item in files:
-    HOME_COPY = join(HOME_DIR, item[1])
-    if isdir(item[0]):
+    if isinstance(item, tuple):
+        ITEM_NAME = item[0]
+        HOME_COPY = join(HOME_DIR, item[1])
+    else:
+        ITEM_NAME = item
+        HOME_COPY = join(HOME_DIR, item)
+
+    if isdir(ITEM_NAME):
         if exists(HOME_COPY):
             rmtree(HOME_COPY)
-        copytree(item[0], HOME_COPY)
+        copytree(ITEM_NAME, HOME_COPY)
     else:
-        copy(item[0], HOME_COPY)
+        copy(ITEM_NAME, HOME_COPY)
 
-source('bashrc')
-source('bash_profile')
+
+def source(filename, dest):
+    SOURCE = 'source ~/{filename}'.format(filename=filename)
+    with open(join(HOME_DIR, '{dest}'.format(dest=dest)), mode='r') as fp:
+        content = fp.read()
+    if SOURCE not in content:
+        with open(join(HOME_DIR, '{dest}'.format(dest=dest)), mode='a') as fp:
+            fp.write('\n' + SOURCE + '\n')
+
+source('.mybashrc', '.bashrc')
+source('.mybash_profile', '.bash_profile')
