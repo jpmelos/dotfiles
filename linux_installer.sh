59c744d32076ff19c9a56833861cfe1cebd788da
João Sampaio <jpmelos@gmail.com>
2018-05-20 21:21:29 -0400

    Install Python 2.7 in Ubuntu 18.04

--- linux_installer.sh
+++ linux_installer.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
-sudo apt-get install git build-essential cmake
+# We install python because Ubuntu 18.04 does not come with
+# Python 2.7 installed by default
+sudo apt-get install -y git build-essential cmake python
 
 mkdir -p ~/devel
 cd ~/devel
