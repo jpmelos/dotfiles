853d55935c0cc9d3cd46ff410ed13283cf73fa6a
João Sampaio <jpmelos@gmail.com>
2018-05-20 21:42:50 -0400

    Add gnome-shell

--- linux_installer.sh
+++ linux_installer.sh
@@ -1,7 +1,12 @@
 sudo apt-get update
 # We install python because Ubuntu 18.04 does not come with
 # Python 2.7 installed by default
-sudo apt-get install -y git build-essential cmake python vim
+sudo apt-get install -y \
+    git build-essential cmake vim gnome-shell \
+    python python-dev python3 python3-dev
+
+# Configure GDM to use vanilla Gnome colors and branding
+sudo update-alternatives --set gdm3.css /usr/share/gnome-shell/theme/gnome-shell.css
 
 mkdir -p ~/devel
 cd ~/devel
