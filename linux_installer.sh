ced7767341871841baa2a6c06f07a2ec7bda621d
João Sampaio <jpmelos@gmail.com>
2018-05-21 09:07:42 -0400

    Add OpenVPN

--- linux_installer.sh
+++ linux_installer.sh
@@ -3,11 +3,14 @@ sudo apt-get update
 # Python 2.7 installed by default
 sudo apt-get install -y \
     gnome-shell build-essential cmake git vim \
+    \
     python python-dev python3 python3-dev \
     libreadline-dev libncursesw5-dev libssl-dev \
     libssl1.0-dev libgdbm-dev libsqlite3-dev \
     libbz2-dev liblzma-dev tk-dev libdb-dev \
-    zlib1g-dev
+    zlib1g-dev \
+    \
+    openvpn network-manager-openvpn-gnome
 
 # Configure GDM to use vanilla Gnome colors and branding
 sudo update-alternatives --set gdm3.css /usr/share/gnome-shell/theme/gnome-shell.css
