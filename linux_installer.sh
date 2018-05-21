494012273eb8b8eb4f449520460ee1a4a9e40c5e
João Sampaio <jpmelos@gmail.com>
2018-05-20 23:02:09 -0400

    Add libssl1.0-dev to Linux installer
    This is required for Python 3.4.8, because it does not support OpenSSL
    1.1.

--- linux_installer.sh
+++ linux_installer.sh
@@ -2,13 +2,12 @@ sudo apt-get update
 # We install python because Ubuntu 18.04 does not come with
 # Python 2.7 installed by default
 sudo apt-get install -y \
-    git build-essential cmake vim gnome-shell \
+    gnome-shell build-essential cmake git vim \
     python python-dev python3 python3-dev \
-    libz-dev libreadline-dev libncursesw5-dev \
-    libssl-dev libgdbm-dev libsqlite3-dev \
+    libreadline-dev libncursesw5-dev libssl-dev \
+    libssl1.0-dev libgdbm-dev libsqlite3-dev \
     libbz2-dev liblzma-dev tk-dev libdb-dev \
     zlib1g-dev
-
 
 # Configure GDM to use vanilla Gnome colors and branding
 sudo update-alternatives --set gdm3.css /usr/share/gnome-shell/theme/gnome-shell.css
