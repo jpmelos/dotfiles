d594fdbfa46832971b6c79389b2e9b9b2b027da7
João Sampaio <jpmelos@gmail.com>
2018-11-24 16:59:33 -0500

    Allow connections from the local network

--- references/mullvad/start_firewall.sh
+++ references/mullvad/start_firewall.sh
@@ -4,9 +4,7 @@ sudo iptables -D INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -D OUTPUT -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 sudo iptables -A INPUT -i tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A INPUT -i wlp2s0 -s 192.168.0.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p udp -m udp -i wlp2s0 -s 177.67.80.186 --sport 1302 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 sudo iptables -A OUTPUT -o tun0 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -o wlp2s0 -d 192.168.0.0/24 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p udp -m udp -o wlp2s0 -d 177.67.80.186 --dport 1302 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
--- references/mullvad/stop_firewall.sh
+++ references/mullvad/stop_firewall.sh
@@ -1,11 +1,9 @@
 sudo cp ~/devel/dotfiles/references/resolv.conf /etc/resolv.conf
 
 sudo iptables -D INPUT -i tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -D INPUT -i wlp2s0 -s 192.168.0.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -D INPUT -p udp -m udp -i wlp2s0 -s 177.67.80.186 --sport 1302 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 sudo iptables -D OUTPUT -o tun0 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -D OUTPUT -o wlp2s0 -d 192.168.0.0/24 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -D OUTPUT -p udp -m udp -o wlp2s0 -d 177.67.80.186 --dport 1302 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
