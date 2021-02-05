f04bfd41343c5a2ee8068b697ba003c5ae55c9b5
João Sampaio <jpmelos@gmail.com>
2021-02-05 08:05:09 -0300

    A few small improvements

--- quality.sh
+++ quality.sh
@@ -1,3 +1,3 @@
 find . -name "*.py" -print0 | xargs -0 isort --profile black
-find . -name "*.py" -print0 | xargs -0 black
+find . -name "*.py" -print0 | xargs -0 black --skip-magic-trailing-comma
 find . -name "*.py" -print0 | xargs -0 flake8
