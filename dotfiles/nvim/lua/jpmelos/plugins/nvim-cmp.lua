6307a4ced65684df2ee1d39b5a8fea312b4f4661
João Sampaio <jpmelos@gmail.com>
2024-05-09 08:23:35 +0100

    `nvim-cmp` breaks without a snippet engine

--- dotfiles/nvim/lua/jpmelos/plugins/nvim-cmp.lua
+++ dotfiles/nvim/lua/jpmelos/plugins/nvim-cmp.lua
@@ -4,20 +4,30 @@ return {
     dependencies = {
         "hrsh7th/cmp-buffer", -- Source for text in buffer.
         "hrsh7th/cmp-path", -- Source for file system paths.
+        { "L3MON4D3/LuaSnip", version = "v2.*" },
+        "saadparwaiz1/cmp_luasnip", -- For integration with `L3MON4D3/LuaSnip`.
         "onsails/lspkind.nvim", -- Pictograms inside suggestions modal.
     },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
         local lspkind = require("lspkind")
 
         cmp.setup({
             completion = {
                 completeopt = "menu,menuone,preview,noselect",
             },
+            -- Configure the snippet engine. This is required, otherwise
+            -- `nvim-cmp` may show weird behavior.
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
-                -- Next suggestion.
-                ["<C-k>"] = cmp.mapping.select_prev_item(),
                 -- Previous suggestion.
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                -- Next suggestion.
                 ["<C-j>"] = cmp.mapping.select_next_item(),
                 -- Scroll documentation pane.
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -30,9 +40,10 @@ return {
                 -- Insert suggestion.
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
             }),
-            -- sources for autocompletion
+            -- Sources for autocompletion.
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
+                { name = "luasnip" }, -- Snippets.
                 { name = "buffer" }, -- Text from the current buffer.
                 { name = "path" }, -- Paths from the file system.
             }),
