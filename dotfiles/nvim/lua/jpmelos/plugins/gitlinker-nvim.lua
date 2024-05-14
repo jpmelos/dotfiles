c899876edd1cdd2a24235c3b3dafc45fa36cb428
João Sampaio <jpmelos@gmail.com>
2024-05-14 22:23:14 +0100

    Format

--- dotfiles/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
+++ dotfiles/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
@@ -5,25 +5,27 @@ return {
         require("gitlinker").setup({ mappgins = nil })
 
         vim.api.nvim_create_user_command("GetGitLink", function()
-            require("gitlinker").get_buf_range_url(
-                "n",
-                {
-                    action_callback =
-                        require("gitlinker.actions").open_in_browser,
-                }
-            )
+            require("gitlinker").get_buf_range_url("n", {
+                action_callback = require("gitlinker.actions").open_in_browser,
+            })
         end, {})
         vim.api.nvim_create_user_command("GetGitLinkV", function()
-            require("gitlinker").get_buf_range_url(
-                "v",
-                {
-                    action_callback =
-                        require("gitlinker.actions").open_in_browser,
-                }
-            )
+            require("gitlinker").get_buf_range_url("v", {
+                action_callback = require("gitlinker.actions").open_in_browser,
+            })
         end, {})
 
-        vim.api.nvim_set_keymap("n", "<leader>gy", "<cmd>GetGitLink<CR>", { desc = "Copy git permalink" })
-        vim.api.nvim_set_keymap("v", "<leader>gy", "<cmd>GetGitLinkV<CR>", { desc = "Copy git permalink" })
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>gy",
+            "<cmd>GetGitLink<CR>",
+            { desc = "Copy git permalink" }
+        )
+        vim.api.nvim_set_keymap(
+            "v",
+            "<leader>gy",
+            "<cmd>GetGitLinkV<CR>",
+            { desc = "Copy git permalink" }
+        )
     end,
 }
