d86459a0089e3661da6c7480bf130d2b73003601
João Sampaio <jpmelos@gmail.com>
2024-05-16 16:12:20 +0100

    Continue tuning the colorscheme

--- dotfiles/nvim/lua/jpmelos/plugins/github-nvim-theme.lua
+++ dotfiles/nvim/lua/jpmelos/plugins/github-nvim-theme.lua
@@ -7,17 +7,12 @@ return {
     -- always set highlight groups in the other plugins' startup logic.
     priority = 1000,
     config = function()
-        require("github-theme").setup({
-            options = {
-                darken = {
-                    floats = true,
-                },
-            },
-        })
+        require("github-theme").setup()
 
         vim.cmd("colorscheme github_light")
         vim.cmd("hi Comment gui=italic guifg=#57606a guibg=none")
         vim.cmd("hi StatusLineNC gui=none guifg=#ffffff guibg=#5fafd7")
         vim.cmd("hi CursorLine gui=underline guifg=none guibg=none")
+        vim.cmd("hi! link @constructor Function")
     end,
 }
