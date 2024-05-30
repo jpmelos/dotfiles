91d77e29665eb9971348a308798b046e2dc59ad7
João Sampaio <jpmelos@gmail.com>
2024-05-30 10:59:46 +0100

    Nvim-tree as a floating window

--- dotfiles/nvim/lua/jpmelos/plugins/nvim-tree.lua
+++ dotfiles/nvim/lua/jpmelos/plugins/nvim-tree.lua
@@ -1,3 +1,6 @@
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
+
 return {
     "nvim-tree/nvim-tree.lua",
     lazy = false,
@@ -9,11 +12,37 @@ return {
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
 
-        require("nvim-tree").setup({
+        local tree = require("nvim-tree")
+        local tree_view = require("nvim-tree.view")
+
+        tree.setup({
             view = {
-                width = 35,
                 number = true,
                 relativenumber = true,
+                float = {
+                    enable = true,
+                    open_win_config = function()
+                        local screen_w = vim.opt.columns:get()
+                        local screen_h = vim.opt.lines:get()
+
+                        local window_w = math.floor(screen_w * WIDTH_RATIO)
+                        local window_h = math.floor(screen_h * HEIGHT_RATIO)
+
+                        local top_x = math.floor((screen_w - window_w) / 2)
+                        local top_y = math.floor((screen_h - window_h) / 2)
+
+                        return {
+                            border = "rounded",
+                            relative = "editor",
+
+                            col = top_x,
+                            width = window_w,
+
+                            row = top_y,
+                            height = window_h,
+                        }
+                    end,
+                },
             },
             renderer = {
                 indent_markers = {
@@ -42,6 +71,17 @@ return {
             git = {
                 ignore = false, -- Do not omit files ignored by git.
             },
+        })
+
+        vim.api.nvim_create_augroup("NvimTreeResize", { clear = true })
+        vim.api.nvim_create_autocmd({ "VimResized" }, {
+            group = "NvimTreeResize",
+            callback = function()
+                if tree_view.is_visible() then
+                    tree_view.close()
+                    tree.open()
+                end
+            end,
         })
 
         K(
