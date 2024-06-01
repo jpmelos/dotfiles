5842bf11c30e47625e91d7de4da56ca52d216b2a
João Sampaio <jpmelos@gmail.com>
2024-06-01 23:16:35 +0100

    Improve todo-comments a bit

--- dotfiles/nvim/lua/jpmelos/plugins/todo-comments-nvim.lua
+++ dotfiles/nvim/lua/jpmelos/plugins/todo-comments-nvim.lua
@@ -7,13 +7,14 @@ return {
 
         local todo_comments = require("todo-comments")
 
-        K("n", "]c", function()
-            todo_comments.jump_next()
-        end, { desc = "Next todo comment" })
-        K("n", "[c", function()
-            todo_comments.jump_prev()
-        end, { desc = "Previous todo comment" })
-
         todo_comments.setup()
+
+        K("n", "]c", todo_comments.jump_next, { desc = "Next todo comment" })
+        K(
+            "n",
+            "[c",
+            todo_comments.jump_prev,
+            { desc = "Previous todo comment" }
+        )
     end,
 }
