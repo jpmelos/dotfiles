be30c402410d4c53d78b010abcacaae3262aafc1
João Sampaio <jpmelos@gmail.com>
2024-06-01 23:28:18 +0100

    Enable treesitter additional text objects

--- dotfiles/nvim/lua/jpmelos/plugins/nvim-treesitter.lua
+++ dotfiles/nvim/lua/jpmelos/plugins/nvim-treesitter.lua
@@ -92,6 +92,27 @@ return {
                 end
                 return false
             end,
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        -- You can use the capture groups defined in
+                        -- textobjects.scm.
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["ii"] = "@conditional.inner",
+                        ["ai"] = "@conditional.outer",
+                        ["il"] = "@loop.inner",
+                        ["al"] = "@loop.outer",
+                        ["at"] = "@comment.outer",
+                    },
+                },
+            },
         })
 
         local opt = vim.opt
