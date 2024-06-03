064f175b49a2fb7242eab99a9c9f060ad871a49a
João Sampaio <jpmelos@gmail.com>
2024-06-03 12:52:10 +0100

    Run black after ruff in closeio

--- autoenvs/devel/close/closeio/.nvim.lua
+++ autoenvs/devel/close/closeio/.nvim.lua
@@ -2,5 +2,5 @@ vim.g.disable_autoformat = false
 
 local conform = require("conform")
 conform.formatters_by_ft = {
-	python = { "black", "ruff_fix", "ruff_organize_imports" },
+	python = { "ruff_fix", "ruff_organize_imports", "black" },
 }
