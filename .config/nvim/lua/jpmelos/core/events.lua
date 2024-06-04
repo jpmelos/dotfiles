a87a5cc96093319b3ecd9be004a82260fbbb30c9
João Sampaio <jpmelos@gmail.com>
2024-06-04 17:36:30 +0100

    No need for workspaces...

--- .config/nvim/lua/jpmelos/core/events.lua
+++ .config/nvim/lua/jpmelos/core/events.lua
@@ -1,3 +1,16 @@
+-- Load local ./.nvim.lua file, if one exists.
+local my_ws_grp = vim.api.nvim_create_augroup("my_ws_grp", { clear = true })
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    callback = function()
+        local exrc_path = vim.fn.getcwd() .. "/.nvim.lua"
+        if vim.secure.read(exrc_path) then
+            vim.cmd.source(exrc_path)
+            vim.notify("Sourced ./.nvim.lua.")
+        end
+    end,
+    group = my_ws_grp,
+})
+
 -- See `:help vim.highlight.on_yank()`.
 local highlight_group =
     vim.api.nvim_create_augroup("YankHighlight", { clear = true })
