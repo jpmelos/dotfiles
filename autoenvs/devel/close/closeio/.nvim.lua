90716133c1adf46baa482eebfa0cfbb479e54a53
João Sampaio <jpmelos@gmail.com>
2024-06-04 10:18:53 +0100

    Manage with stow!!

--- autoenvs/devel/close/closeio/.nvim.lua
+++
@@ -1,6 +0,0 @@
-vim.g.disable_autoformat = false
-
-local conform = require("conform")
-conform.formatters_by_ft = {
-	python = { "ruff_fix", "ruff_organize_imports", "black" },
-}
--- autoenvs/devel/close/closeio/jpenv-test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-. .autoenv.enter
-
-ftd $@
