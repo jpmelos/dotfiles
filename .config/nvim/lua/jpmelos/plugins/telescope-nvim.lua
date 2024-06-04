04ccbe3c2f35df7d09ecdde0b131daae17ee4366
João Sampaio <jpmelos@gmail.com>
2024-06-04 12:06:53 +0100

    Improve telescope pickers, show hidden files

--- .config/nvim/lua/jpmelos/plugins/telescope-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/telescope-nvim.lua
@@ -35,6 +35,18 @@ return {
 
         telescope.setup({
             defaults = {
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                    "--glob",
+                    "!/.git",
+                },
                 path_display = { "smart" },
                 mappings = {
                     n = {
