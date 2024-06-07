26df791cf3105b8afd0b31e22ddfdf3b9c8c40bd
João Sampaio <jpmelos@gmail.com>
2024-06-07 14:30:32 +0100

    No signs for todo comments

--- .config/nvim/lua/jpmelos/plugins/todo-comments-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/todo-comments-nvim.lua
@@ -7,7 +7,7 @@ return {
 
         local todo_comments = require("todo-comments")
 
-        todo_comments.setup()
+        todo_comments.setup({ signs = false })
 
         K("n", "]c", todo_comments.jump_next, { desc = "Next todo comment" })
         K(
