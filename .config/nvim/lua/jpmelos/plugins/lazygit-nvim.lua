c492fec876f54095b307ad292329b808692a6dcd
João Sampaio <jpmelos@gmail.com>
2024-09-06 09:31:47 +0100

    Upgrade dependencies and install LazyGit

---
+++ .config/nvim/lua/jpmelos/plugins/lazygit-nvim.lua
@@ -0,0 +1,6 @@
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = { { "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open LazyGit" } },
+}
