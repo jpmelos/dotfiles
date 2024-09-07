5acda5bfbc7f5a6a32bff1262f6e96e1cec72c1e
João Sampaio <jpmelos@gmail.com>
2024-09-07 10:56:36 +0100

    Explicitly mention why we need shellcheck

--- .config/nvim/lua/jpmelos/plugins/mason-tool-installer-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/mason-tool-installer-nvim.lua
@@ -8,6 +8,8 @@ return {
         require("mason-tool-installer").setup({
             -- Install language servers with `mason-lspconfig.nvim` to
             -- ensure automatic configuration.
+            -- shellcheck: Not explicitly referenced in these dotfiles, but
+            -- it's used by `bashls` to provide some of its functionalities.
             ensure_installed = { "shellcheck", "shfmt", "ruff", "stylua" },
         })
     end,
