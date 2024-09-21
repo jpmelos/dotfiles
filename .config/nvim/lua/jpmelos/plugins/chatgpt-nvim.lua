cffc21e50737b9698454c64e1b7b5cee86604b30
João Sampaio <jpmelos@gmail.com>
2024-09-21 09:53:00 +0100

    Improve ChatGPT integration

--- .config/nvim/lua/jpmelos/plugins/chatgpt-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/chatgpt-nvim.lua
@@ -1,3 +1,12 @@
+local openai_params = {
+    model = "gpt-4o",
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    temperature = 0,
+    top_p = 1,
+    n = 1,
+}
+
 return {
     "jackMort/ChatGPT.nvim",
     lazy = false,
@@ -8,28 +17,21 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     config = function()
+        local K = vim.keymap.set
+
         require("chatgpt").setup({
             api_key_cmd = "op item get vtzbgc3ybgalzoamj7hietafpe --vault afpmshswzblkzmziqhtqfu2sji --fields notesPlain",
             show_line_numbers = true,
-            openai_params = {
-                model = "gpt-4o",
-                frequency_penalty = 0,
-                presence_penalty = 0,
-                temperature = 0,
-                top_p = 1,
-                n = 1,
-            },
-            openai_edit_params = {
-                model = "gpt-4o",
-                frequency_penalty = 0,
-                presence_penalty = 0,
-                temperature = 0,
-                top_p = 1,
-                n = 1,
-            },
-            use_openai_functions_for_edits = false,
-            actions_paths = {},
-            predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
+            openai_params = openai_params,
+            openai_edit_params = openai_params,
         })
+
+        K({ "n" }, "<leader>ao", "<cmd>ChatGPT<CR>", { desc = "Open chat" })
+        K(
+            { "v" },
+            "<leader>ae",
+            "<cmd>ChatGPTEditWithInstructions<CR>",
+            { desc = "Edit code" }
+        )
     end,
 }
