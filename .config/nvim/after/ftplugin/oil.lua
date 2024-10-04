8488b1607501d00427bef94a46d85c3041a51d76
João Sampaio <jpmelos@gmail.com>
2024-10-04 15:35:28 +0100

    Select easily in Oil.nvim. Again.

--- .config/nvim/after/ftplugin/oil.lua
+++ .config/nvim/after/ftplugin/oil.lua
@@ -1,4 +1,5 @@
 local K = vim.keymap.set
 local actions = require("oil.actions")
 
+K("n", "<CR>", actions.select.callback, { buffer = true })
 K({ "n", "i" }, "<C-c>", "<CMD>q<CR>", { buffer = true })
