9903c3c41ca7053dfb2fea10b36ccb80ad4df593
João Sampaio <jpmelos@gmail.com>
2024-12-10 18:34:29 +0000

    Fix `.nvim.lua`

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,13 @@
-vim.g.disable_autoformat = false
+local g = vim.g
+local api = vim.api
+local ol = vim.opt_local
+
+g.enable_autoformat = true
+
+-- Specific color column setting for Lua files.
+api.nvim_create_autocmd("FileType", {
+    pattern = "lua",
+    callback = function()
+        ol.colorcolumn = "80"
+    end,
+})
