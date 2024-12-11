6701f9027a8584d9e0f2ea5447daadad520e5226
João Sampaio <jpmelos@gmail.com>
2024-12-11 10:34:48 +0000

    Drop `lazygit.nvim`

--- .config/nvim/lua/jpmelos/plugins/lazygit-nvim.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = { { "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open LazyGit" } },
-}
