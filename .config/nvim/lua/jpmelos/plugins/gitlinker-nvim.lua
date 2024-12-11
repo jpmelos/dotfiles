26d6bfea163ab74a207acea2e1afd8cb5afd6a21
João Sampaio <jpmelos@gmail.com>
2024-12-11 10:09:44 +0000

    Put git permalinks in clipboard when opening

--- .config/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
@@ -8,12 +8,18 @@ return {
 
         vim.api.nvim_create_user_command("GetGitLink", function()
             require("gitlinker").get_buf_range_url("n", {
-                action_callback = require("gitlinker.actions").open_in_browser,
+                action_callback = function(url)
+                    vim.cmd('let @+ = "' .. url .. '"')
+                    require("gitlinker.actions").open_in_browser(url)
+                end,
             })
         end, {})
         vim.api.nvim_create_user_command("GetGitLinkV", function()
             require("gitlinker").get_buf_range_url("v", {
-                action_callback = require("gitlinker.actions").open_in_browser,
+                action_callback = function(url)
+                    vim.cmd('let @+ = "' .. url .. '"')
+                    require("gitlinker.actions").open_in_browser(url)
+                end,
             })
         end, {})
 
@@ -23,23 +29,13 @@ return {
             "<cmd>GetGitLink<CR>",
             { desc = "Copy permalink" }
         )
-        K(
-            "n",
-            "gy",
-            "<cmd>GetGitLink<CR>",
-            { desc = "Copy permalink" }
-        )
+        K("n", "gy", "<cmd>GetGitLink<CR>", { desc = "Copy permalink" })
         K(
             "v",
             "<leader>gy",
             "<cmd>GetGitLinkV<CR>",
             { desc = "Copy permalink" }
         )
-        K(
-            "v",
-            "gy",
-            "<cmd>GetGitLinkV<CR>",
-            { desc = "Copy permalink" }
-        )
+        K("v", "gy", "<cmd>GetGitLinkV<CR>", { desc = "Copy permalink" })
     end,
 }
