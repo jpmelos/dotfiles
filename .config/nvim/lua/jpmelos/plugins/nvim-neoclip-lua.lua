86f1a7aee48605ef2cf1ad409e2f7f5ea166a310
João Sampaio <jpmelos@gmail.com>
2025-03-14 15:36:21 -0400

    Improve `neoclip.nvim` configuration

--- .config/nvim/lua/jpmelos/plugins/nvim-neoclip-lua.lua
+++ .config/nvim/lua/jpmelos/plugins/nvim-neoclip-lua.lua
@@ -12,6 +12,32 @@ return {
             enable_persistent_history = true,
             continuous_sync = true,
             default_register = "+",
+            on_select = {
+                move_to_front = true,
+                close_telescope = true,
+            },
+            keys = {
+                telescope = {
+                    i = {
+                        --- It is possible to map to more than one key.
+                        -- select = { '<CR>', '<C-C>' }
+                        select = "<CR>",
+                        delete = "<C-D>", -- delete an entry
+                        edit = "<C-E>", -- edit an entry
+                        paste = "<NOP>",
+                        paste_behind = "<NOP>",
+                        replay = "<NOP>",
+                    },
+                    n = {
+                        select = "<CR>",
+                        delete = "D",
+                        edit = "E",
+                        paste = "<NOP>",
+                        paste_behind = "<NOP>",
+                        replay = "<NOP>",
+                    },
+                },
+            },
         })
 
         K(
