6dde7d1f7a24b9a426c89d17bda3956a01746eb7
João Sampaio <jpmelos@gmail.com>
2025-03-14 15:59:18 -0400

    Document how `vim-radical` works

--- .config/nvim/lua/jpmelos/plugins/vim-radical.lua
+++ .config/nvim/lua/jpmelos/plugins/vim-radical.lua
@@ -1,1 +1,6 @@
+-- Converts numbers between bases.
+-- `gA` shows the four representations of the number under the cursor (or
+-- selected in Visual mode).
+-- `crd`, `crx`, `cro`, `crb` converts the number under the cursor to decimal,
+-- hex, octal, or binary, respectively.
 return { "glts/vim-radical", dependencies = { "glts/vim-magnum" } }
