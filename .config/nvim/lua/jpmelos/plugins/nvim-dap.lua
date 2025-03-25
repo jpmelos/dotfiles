a22ff88752f86b8a93fa82558eef2992f8bbb940
João Sampaio <jpmelos@gmail.com>
2025-03-25 09:32:17 -0400

    DAP improvements

--- .config/nvim/lua/jpmelos/plugins/nvim-dap.lua
+++ .config/nvim/lua/jpmelos/plugins/nvim-dap.lua
@@ -1,5 +1,4 @@
-local already_in_session = false
-local waiting_for_breakpoint = nil
+local saved_breakpoint = nil
 
 local function broadcast(sessions, fn)
     for _, session in pairs(sessions) do
@@ -8,82 +7,107 @@ local function broadcast(sessions, fn)
     end
 end
 
-local function set_saved_bp_and_run(ask_for_condition)
-    local api = vim.api
-    local dap = require("dap")
+local function ask_for_breakpoint_condition(on_submit_cb)
     local Input = require("nui.input")
     local event = require("nui.utils.autocmd").event
 
-    if not already_in_session then
-        OpenCurrentBufferInNewTab()
-        already_in_session = true
+    local input = Input({
+        position = "50%",
+        size = { width = 80 },
+        border = {
+            style = "rounded",
+            text = {
+                top = " Breakpoint Condition ",
+                top_align = "center",
+            },
+        },
+        win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:Normal",
+        },
+    }, {
+        prompt = "> ",
+        on_submit = on_submit_cb,
+    })
+
+    input:on(event.BufLeave, function()
+        input:unmount()
+    end)
+
+    input:map("n", "<C-c>", function()
+        input:unmount()
+    end, { noremap = true })
+    input:map("i", "<C-c>", function()
+        input:unmount()
+    end, { noremap = true })
+
+    input:mount()
+end
+
+local function toggle_breakpoint(ask_for_condition)
+    local dap = require("dap")
+
+    if ask_for_condition then
+        ask_for_breakpoint_condition(function(condition)
+            local condition_hit = tonumber(condition)
+            if condition_hit then
+                dap.toggle_breakpoint(nil, condition)
+                return
+            end
+
+            if condition ~= "" and condition ~= nil then
+                dap.toggle_breakpoint(condition)
+                return
+            end
+
+            dap.toggle_breakpoint()
+        end)
+    else
+        dap.toggle_breakpoint()
     end
+end
+
+local function set_saved_breakpoint_and_run(ask_for_condition)
+    local api = vim.api
+    local dap = require("dap")
 
     local bufnr = api.nvim_get_current_buf()
     local lnum = api.nvim_win_get_cursor(0)[1]
-    waiting_for_breakpoint = { bufnr = bufnr, lnum = lnum }
+    saved_breakpoint = { bufnr = bufnr, lnum = lnum }
 
     if ask_for_condition then
-        local input = Input({
-            position = "50%",
-            size = { width = 80 },
-            border = {
-                style = "rounded",
-                text = {
-                    top = " Breakpoint Condition ",
-                    top_align = "center",
-                },
-            },
-            win_options = {
-                winhighlight = "Normal:Normal,FloatBorder:Normal",
-            },
-        }, {
-            prompt = "> ",
-            on_submit = function(condition)
-                local condition_hit = tonumber(condition)
-                if condition_hit then
-                    dap.set_breakpoint(nil, condition)
-                    dap.continue()
-                    return
-                end
-
-                if condition ~= "" and condition ~= nil then
-                    dap.set_breakpoint(condition)
-                    dap.continue()
-                    return
-                end
-
-                dap.set_breakpoint()
+        ask_for_breakpoint_condition(function(condition)
+            local condition_hit = tonumber(condition)
+            if condition_hit then
+                dap.set_breakpoint(nil, condition)
                 dap.continue()
-            end,
-        })
-        input:on(event.BufLeave, function()
-            input:unmount()
+                return
+            end
+
+            if condition ~= "" and condition ~= nil then
+                dap.set_breakpoint(condition)
+                dap.continue()
+                return
+            end
+
+            dap.set_breakpoint()
+            dap.continue()
         end)
-        input:map("n", "<C-c>", function()
-            input:unmount()
-        end, { noremap = true })
-        input:map("i", "<C-c>", function()
-            input:unmount()
-        end, { noremap = true })
-        input:mount()
     else
         dap.set_breakpoint()
         dap.continue()
     end
 end
 
-local function unset_saved_bp()
+local function unset_saved_breakpoint()
     local dap = require("dap")
     local dap_breakpoints = require("dap.breakpoints")
 
-    if waiting_for_breakpoint == nil then
-        -- Nothing to do.
+    if saved_breakpoint == nil then
         return
     end
 
-    local bufnr = waiting_for_breakpoint.bufnr
-    local lnum = waiting_for_breakpoint.lnum
+    local bufnr = saved_breakpoint.bufnr
+    local lnum = saved_breakpoint.lnum
 
     dap_breakpoints.toggle({ replace = true }, bufnr, lnum)
     dap_breakpoints.toggle({}, bufnr, lnum)
@@ -93,19 +117,24 @@ local function unset_saved_bp()
     end)
 end
 
-local register_waiting_for_breakpoint_handler = function()
+local function register_event_handlers()
     local dap = require("dap")
 
-    local handle_stopped_event = function()
-        unset_saved_bp()
+    local function handle_initialized_event()
+        OpenCurrentBufferInNewTab()
     end
 
-    local handle_terminated_event = function()
-        unset_saved_bp()
+    local function handle_stopped_event()
+        unset_saved_breakpoint()
+    end
+
+    local function handle_terminated_event()
+        unset_saved_breakpoint()
         vim.cmd("tabclose")
-        already_in_session = false
     end
 
+    dap.listeners.before["event_initialized"]["jpmelos"] =
+        handle_initialized_event
     dap.listeners.before["event_stopped"]["jpmelos"] = handle_stopped_event
     dap.listeners.before["event_terminated"]["jpmelos"] =
         handle_terminated_event
@@ -233,23 +262,21 @@ return {
         )
 
         -- Register handler to handle our `run_to_cursor`.
-        register_waiting_for_breakpoint_handler()
+        register_event_handlers()
         -- This is our custom `run_to_cursor` mapping. It works even when there
         -- isn't a session running: it starts a session.
         K("n", "<leader>dr", function()
-            set_saved_bp_and_run()
+            set_saved_breakpoint_and_run()
         end, { desc = "Run to cursor" })
         K("n", "<leader>dR", function()
-            set_saved_bp_and_run(true)
-        end, { desc = "Run to cursor" })
+            set_saved_breakpoint_and_run(true)
+        end, { desc = "Run to cursor conditionally" })
         K("n", "<leader>dx", dap.disconnect, { desc = "End current session" })
 
-        K(
-            "n",
-            "<leader>db",
-            dap.toggle_breakpoint,
-            { desc = "Toggle breakpoint" }
-        )
+        K("n", "<leader>db", toggle_breakpoint, { desc = "Toggle breakpoint" })
+        K("n", "<leader>dB", function()
+            toggle_breakpoint(true)
+        end, { desc = "Toggle conditional breakpoint" })
 
         K({ "n", "v" }, "<leader>dv", function()
             vim.cmd("vertical DapEval")
