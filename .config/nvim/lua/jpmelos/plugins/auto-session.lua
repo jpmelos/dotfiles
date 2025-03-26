e63b440122379cc2d399710293ced2b2cc90b038
João Sampaio <jpmelos@gmail.com>
2025-03-25 22:48:49 -0400

    Load `rmagatti/auto-session` lazily

--- .config/nvim/lua/jpmelos/plugins/auto-session.lua
+++ .config/nvim/lua/jpmelos/plugins/auto-session.lua
@@ -1,32 +1,27 @@
 return {
     "rmagatti/auto-session",
-    config = function()
-        local K = vim.keymap.set
-
-        require("auto-session").setup({
-            -- Automatic behavior.
-            enabled = false,
-            auto_save = false,
-            auto_restore = false,
-            auto_create = false,
-            -- Do not restore last session if a session for the current
-            -- directory can't be found.
-            auto_restore_last_session = false,
-            -- Do not base sessions on git branches.
-            use_git_branch = false,
-        })
-
-        K(
-            "n",
+    keys = {
+        {
             "<leader>wr",
             "<cmd>SessionRestore<CR>",
-            { desc = "Restore session for current directory" }
-        )
-        K(
-            "n",
+            desc = "Restore session for current directory",
+        },
+        {
             "<leader>ws",
             "<cmd>SessionSave<CR>",
-            { desc = "Save session for current directory" }
-        )
-    end,
+            desc = "Save session for current directory",
+        },
+    },
+    opts = {
+        -- Automatic behavior.
+        enabled = false,
+        auto_save = false,
+        auto_restore = false,
+        auto_create = false,
+        -- Do not restore last session if a session for the current
+        -- directory can't be found.
+        auto_restore_last_session = false,
+        -- Do not base sessions on git branches.
+        use_git_branch = false,
+    },
 }
