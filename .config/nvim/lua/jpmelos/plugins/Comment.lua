f839ef0669fe626409c8ac2389ebdc168708fddb
João Sampaio <jpmelos@gmail.com>
2025-03-29 11:28:29 -0400

    Make `Comment.lua` load lazily

--- .config/nvim/lua/jpmelos/plugins/Comment.lua
+++ .config/nvim/lua/jpmelos/plugins/Comment.lua
@@ -1,12 +1,13 @@
 return {
     "numToStr/Comment.nvim",
+    keys = { "gcc", "gbb", "gc", "gb", "gcO", "gco", "gcA" },
     dependencies = {
         -- Identify correct comment syntax based on current context, with the
         -- help of tree-sitter. Supports nested languages, for example.
         "JoosepAlviste/nvim-ts-context-commentstring",
     },
-    config = function()
-        require("Comment").setup({
+    opts = function()
+        return {
             -- Normal mode.
             toggler = {
                 line = "gcc",
@@ -29,6 +30,6 @@ return {
             pre_hook = require(
                 "ts_context_commentstring.integrations.comment_nvim"
             ).create_pre_hook(),
-        })
+        }
     end,
 }
