83ec5ad3ffcf244c11cc35e857fb22a7f3e738b9
João Sampaio <jpmelos@gmail.com>
2025-04-02 22:59:05 -0400

    Add a few string utilities

--- .config/nvim/lua/jpmelos/core/strings.lua
+++ .config/nvim/lua/jpmelos/core/strings.lua
@@ -1,7 +1,25 @@
-function string.startswith(str, start)
-    return str.sub(str, 1, str.len(start)) == start
+function string.startswith(str, prefix)
+    return str:sub(1, str.len(prefix)) == prefix
+end
+
+function string.endswith(str, suffix)
+    return str:sub(-#suffix) == suffix
 end
 
 function string.trim(str)
     return str:match("^%s*(.-)%s*$")
+end
+
+function string.split(str, delimiter)
+    local parts = {}
+    for part in str:gmatch("([^" .. delimiter .. "]*)?") do
+        if part ~= "" then
+            table.insert(parts, part)
+        end
+    end
+    return parts
+end
+
+function string.join(delimiter, seq)
+    return table.concat(seq, delimiter)
 end
