c3483b1aefc02a26bbbf67e9242f6ea97a424223
João Sampaio <jpmelos@gmail.com>
2025-04-03 23:42:46 -0400

    Simplify `gitlinker.nvim` code a little bit

--- .config/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
@@ -6,23 +6,20 @@ return {
         require("gitlinker").setup({ mappgins = nil })
 
         vim.api.nvim_create_user_command("GetGitLink", function()
-            require("gitlinker").get_buf_range_url("n", {
+            require("gitlinker").get_buf_range_url(vim.fn.mode():lower(), {
                 action_callback = function(url)
                     vim.cmd('let @+ = "' .. url .. '"')
                     require("gitlinker.actions").open_in_browser(url)
-                end,
-            })
-        end, {})
-        vim.api.nvim_create_user_command("GetGitLinkV", function()
-            require("gitlinker").get_buf_range_url("v", {
-                action_callback = function(url)
-                    vim.cmd('let @+ = "' .. url .. '"')
-                    require("gitlinker.actions").open_in_browser(url)
+                    NormalMode()
                 end,
             })
         end, {})
 
-        K("n", "gy", "<cmd>GetGitLink<CR>", { desc = "Copy permalink" })
-        K("v", "gy", "<cmd>GetGitLinkV<CR>", { desc = "Copy permalink" })
+        K(
+            { "n", "v" },
+            "gy",
+            "<cmd>GetGitLink<CR>",
+            { desc = "Copy permalink" }
+        )
     end,
 }
