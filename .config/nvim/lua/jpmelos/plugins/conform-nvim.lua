be26de1a792879f617c70523ec0089417caae7b6
João Sampaio <jpmelos@gmail.com>
2025-04-03 21:37:11 -0400

    Simplify some code

--- .config/nvim/lua/jpmelos/plugins/conform-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/conform-nvim.lua
@@ -127,15 +127,9 @@ return {
         api.nvim_create_user_command("Format", function(args)
             local range = nil
             if args.count ~= -1 then
-                local end_line = api.nvim_buf_get_lines(
-                    0,
-                    args.line2 - 1,
-                    args.line2,
-                    true
-                )[1]
                 range = {
                     start = { args.line1, 0 },
-                    ["end"] = { args.line2, end_line:len() },
+                    ["end"] = { args.line2, math.maxinteger },
                 }
             end
             conform.format({
