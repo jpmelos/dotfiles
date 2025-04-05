1e0af2ad8d9927506a0a2dde47244abc72bb91c1
João Sampaio <jpmelos@gmail.com>
2025-04-05 11:15:47 -0400

    Improve how we run `.nvim.lua`

--- .config/nvim/lua/jpmelos/core/init.lua
+++ .config/nvim/lua/jpmelos/core/init.lua
@@ -14,7 +14,20 @@ vim.opt.rtp:prepend(vim.g.lazypath)
 
 -- Load local ./.nvim.lua file, if one exists.
 local exrc_path = vim.fn.getcwd() .. "/.nvim.lua"
-if vim.secure.read(exrc_path) then
-    vim.cmd.source(exrc_path)
-    vim.notify("Sourced ./.nvim.lua.")
+local content = vim.secure.read(exrc_path):trim()
+if content and content ~= "" then
+    local chunk, err = load(content, "@" .. exrc_path, "t")
+    if chunk then
+        local ok, exec_err = pcall(chunk)
+        if ok then
+            vim.notify("Sourced .nvim.lua")
+        else
+            vim.notify(
+                "Error executing .nvim.lua: " .. exec_err,
+                vim.log.levels.ERROR
+            )
+        end
+    else
+        vim.notify("Error loading .nvim.lua: " .. err, vim.log.levels.ERROR)
+    end
 end
