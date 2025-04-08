8023869eb983551f30a9be4a6b1069154f1c9ffe
João Sampaio <jpmelos@gmail.com>
2025-04-08 16:03:27 -0400

    Only source `.nvim.lua` if it exists

--- .config/nvim/lua/jpmelos/core/init.lua
+++ .config/nvim/lua/jpmelos/core/init.lua
@@ -12,22 +12,32 @@ require("jpmelos.core.keymaps")
 vim.g.lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(vim.g.lazypath)
 
--- Load local ./.nvim.lua file, if one exists.
-local exrc_path = vim.fn.getcwd() .. "/.nvim.lua"
-local content = vim.secure.read(exrc_path):trim()
-if content and content ~= "" then
-    local chunk, err = load(content, "@" .. exrc_path, "t")
-    if chunk then
-        local ok, exec_err = pcall(chunk)
-        if ok then
-            vim.notify("Sourced .nvim.lua")
+local function source_nvim_lua_file()
+    -- Load local ./.nvim.lua file, if one exists.
+    local exrc_path = vim.fn.getcwd() .. "/.nvim.lua"
+    if vim.fn.filereadable(exrc_path) ~= 1 then
+        return
+    end
+
+    local content = vim.secure.read(exrc_path):trim()
+    if content and content ~= "" then
+        local chunk, err = load(content, "@" .. exrc_path, "t")
+        if chunk then
+            local ok, exec_err = pcall(chunk)
+            if ok then
+                vim.notify("Sourced .nvim.lua")
+            else
+                vim.notify(
+                    "Error executing .nvim.lua: " .. exec_err,
+                    vim.log.levels.ERROR
+                )
+            end
         else
             vim.notify(
-                "Error executing .nvim.lua: " .. exec_err,
+                "Error loading .nvim.lua: " .. err,
                 vim.log.levels.ERROR
             )
         end
-    else
-        vim.notify("Error loading .nvim.lua: " .. err, vim.log.levels.ERROR)
     end
 end
+source_nvim_lua_file()
