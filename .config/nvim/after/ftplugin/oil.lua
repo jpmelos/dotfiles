a9eec75563c8596194466ab71e920885574262e9
João Sampaio <jpmelos@gmail.com>
2025-04-29 20:49:30 -0400

    Use `q` to leave oil.nvim

--- .config/nvim/after/ftplugin/oil.lua
+++ .config/nvim/after/ftplugin/oil.lua
@@ -2,4 +2,4 @@ local K = vim.keymap.set
 local actions = require("oil.actions")
 
 K("n", "<CR>", actions.select.callback, { buffer = true })
-K({ "n", "i" }, "<C-c>", "<CMD>q<CR>", { buffer = true })
+K("n", "q", "<CMD>q<CR>", { buffer = true })
