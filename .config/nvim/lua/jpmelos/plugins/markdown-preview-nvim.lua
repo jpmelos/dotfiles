4d640c8d8a2a9662aec3e4f0eba661b165451324
João Sampaio <jpmelos@gmail.com>
2025-05-27 10:35:30 -0400

    Fix `markdown-preview.nvim`

--- .config/nvim/lua/jpmelos/plugins/markdown-preview-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/markdown-preview-nvim.lua
@@ -1,16 +1,21 @@
 return {
     "iamcco/markdown-preview.nvim",
+    -- This plugin doesn't work when lazy-loaded.
+    lazy = false,
     keys = {
         {
             "<leader>mp",
-            "<cmd>MarkdownPreviewToggle<cr>",
+            "<cmd>MarkdownPreview<cr>",
             mode = "n",
             desc = "Toggle Markdown preview",
         },
     },
+    cmd = {
+        "MarkdownPreviewToggle",
+        "MarkdownPreview",
+        "MarkdownPreviewStop",
+    },
     init = function()
-        local K = vim.keymap.set
-
         -- Do not auto-close the preview window when closing Markdown buffer.
         vim.g.mkdp_auto_close = false
         -- Use the same window for all previews.
