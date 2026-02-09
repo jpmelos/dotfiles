de5d763f2f91c479c8746f87f8f0f68fb8ebf52a
João Sampaio <jpmelos@gmail.com>
2026-02-09 11:43:11 -0500

    Fix single-line selections for Git links

--- .config/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/gitlinker-nvim.lua
@@ -8,6 +8,8 @@ return {
         vim.api.nvim_create_user_command("GetGitLink", function()
             require("gitlinker").get_buf_range_url(vim.fn.mode():lower(), {
                 action_callback = function(url)
+                    -- Fix single-line ranges: #LX-LX should become #LX.
+                    url = url:gsub("#L(%d+)%-L%1", "#L%1")
                     vim.cmd('let @+ = "' .. url .. '"')
                     require("gitlinker.actions").open_in_browser(url)
                     NormalMode()
