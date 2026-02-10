a37f03f7a4f3f91e614f9dbf1313ed70e436c478
João Sampaio <jpmelos@gmail.com>
2026-02-10 11:30:53 -0500

    Simplify `oil.nvim` setup

--- .config/nvim/lua/jpmelos/plugins/oil-nvim.lua
+++ .config/nvim/lua/jpmelos/plugins/oil-nvim.lua
@@ -18,18 +18,7 @@ return {
                 ["g?"] = { "actions.show_help", mode = "n" },
                 ["<CR>"] = "actions.select",
                 ["<C-r>"] = "actions.refresh",
-                ["<C-v>"] = { "actions.select", opts = { vertical = true } },
-                ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
-                ["<C-t>"] = { "actions.select", opts = { tab = true } },
                 ["<C-c>"] = { "actions.close", mode = "n" },
-                ["-"] = { "actions.parent", mode = "n" },
-                ["_"] = { "actions.open_cwd", mode = "n" },
-                ["`"] = { "actions.cd", mode = "n" },
-                ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-                ["gs"] = { "actions.change_sort", mode = "n" },
-                ["gx"] = "actions.open_external",
-                ["g."] = { "actions.toggle_hidden", mode = "n" },
-                ["g\\"] = { "actions.toggle_trash", mode = "n" },
             },
         })
 
